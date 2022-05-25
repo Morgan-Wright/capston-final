7e026ce8df1adc31b0394324c81087e083050620
Morgan-Wright <morgan.q.wright1217@gmail.com>
2022-05-24 20:17:13 -0700

    utils/db complete

---
+++ server/controller/protected.controller.js
@@ -0,0 +1,26 @@
+const rollbar = require("../utils/rollbar")
+const sequelize = require('../utils/db')
+
+async function getList(req, res) {
+    const userID = req.session.user.id;
+  
+    try {
+      const [result] = await sequelize.query(`
+        select * from lists where user_id = ${userID}
+      `);
+  
+      res.status(200).send(result);
+    } catch (error) {
+      const user = req.session.user; // This information is here because I added to the session in my auth.controller.js
+  
+      rollbar.error(error, {
+        user: user,
+      });
+  
+      res.sendStatus(500);
+    }
+  }
+
+module.exports = {
+    getList,
+}
