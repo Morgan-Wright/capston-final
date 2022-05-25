51fc75ee79ee97fac44c75950a509098a27b8500
Morgan-Wright <morgan.q.wright1217@gmail.com>
2022-05-24 20:24:38 -0700

    seed.controller.js complete

---
+++ server/controller/seed.controler.js
@@ -0,0 +1,38 @@
+const sequelize = require('../utils/db')
+
+async function createDatabase(req, res) {
+    try {
+      await sequelize.query(`
+      drop table if exists users;
+      drop table if exists characters;
+      
+      create table users (
+        id serial primary key,
+        username text,
+        hash text
+      );
+
+      create table lists (
+        id serial primary key,
+        content text,
+        user_id int references users(id)
+      );
+
+      create table lists_item (
+          id serial primary key,
+          content text,
+          list_id int references lists_id
+      );
+    `);
+  
+      res.sendStatus(200);
+    } catch (error) {
+      console.log(error);
+  
+      res.sendStatus(500);
+    }
+  }
+  
+  module.exports = {
+    createDatabase,
+  };
